e93f95240c82b78879d6084ed2315e903def9ff0
pcsmomo <dongwoo_333@hanmail.net>
2021-11-25 16:29:45 +1100

    100. Primitives vs. Objects in Practice

--- 08-js-behind-scenes/4-primitive-reference-types.js
+++ 08-js-behind-scenes/4-primitive-reference-types.js
@@ -1,16 +1,36 @@
 'use strict';
 
-let age = 30;
-let oldAge = age;
-// age = 31;
-console.log(age);
-console.log(oldAge);
+// Primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+// console.log(lastName, oldLastName);
 
-const me = {
-  name: 'Jonas',
-  age: 30,
+// Reference types
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
 };
-const friend = me;
-friend.age = 27;
-console.log('Friend:', friend);
-console.log('Me:', me);
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+// console.log('Before married', jessica);
+// console.log('After married', marriedJessica);
+
+// marriedJessica = {};
+
+// Copying objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+const jessicaCopy = Object.assign({}, jessica2); // shallow copy
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before married', jessica2);
+console.log('After married', jessicaCopy);
